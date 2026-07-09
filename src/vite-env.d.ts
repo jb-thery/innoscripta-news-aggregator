37bc5824a89541d1743ac6c96502b3c5d9d35e42
Jean-Baptiste THERY <jb.thery@jcode.works>
2026-07-10 05:26:04 +0700

    fix(tooling): enforce complete TypeScript checks

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,7 @@
 /// <reference types="vite/client" />
 
+declare module "@fontsource-variable/dm-sans"
+declare module "@fontsource-variable/newsreader"
+
 declare const __HASH_ROUTING__: boolean
 declare const __STATIC_MOCK__: boolean
